890c19dfa563dee37e299cb3939927f202d1c08e
Arthuralentejo <arthur.alentejo228@gmail.com>
2021-08-18 19:51:04 -0300

    Adicionando o modo de edição

--- create.sql
+++ create.sql
@@ -1,2 +1,1 @@
 CREATE TABLE facilitadores (id INTEGER PRIMARY KEY AUTOINCREMENT, nome text NOT NULL, email text NOT NULL, cel text NOT NULL, nasc text NOT NULL);
-SELECT id,nome,email,cel,nasc FROM facilitadores;
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import sqlite3
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -30,7 +30,7 @@ def send_data():
     elif request.method == "POST":
         nome = request.form['nome']
         email = request.form['email']
-        cel = request.form['celular']
+        cel = request.form['cel']
         nasc = request.form['nasc']
         conn = sqlite3.connect('facilitadores.db')
         c = conn.cursor()
@@ -55,28 +55,67 @@ def show_data(id):
     conn.commit()
     return render_template("view.html", item = item)
 
+@app.route('/edit/<int:id>/', methods = ['POST','GET'])
+def edit(id):
+    if request.method == "GET":
+        conn = sqlite3.connect('facilitadores.db')
+        c = conn.cursor()
+        c.execute("SELECT name FROM pragma_table_info('facilitadores')")
+        keys = c.fetchall()
+        c.execute("SELECT * FROM facilitadores WHERE id = {}".format(id))
+        row = c.fetchall()
+        conn.commit()
+        key_list = []
+        for i in keys:
+            key_list.append(i[0])
+        item = {}
+        for i in range(len(key_list)):
+            item[key_list[i]] = row[0][i]
+        return render_template("edit.html", item = item)
+    elif request.method == "POST":
+        conn = sqlite3.connect('facilitadores.db')
+        c = conn.cursor()
+        c.execute("SELECT name FROM pragma_table_info('facilitadores')")
+        keys = c.fetchall()
+        c.execute("SELECT * FROM facilitadores WHERE id = {}".format(id))
+        row = c.fetchall()
+        conn.commit()
+        key_list = []
+        for i in keys:
+            key_list.append(i[0])
+        item = {}
+        for i in range(len(key_list)):
+            item[key_list[i]] = row[0][i]
+        for key in key_list:
+            campo = request.form.get(key)
+            if campo != item[key] and campo != None:
+                item[key] = campo
+                c.execute("UPDATE facilitadores SET {} = '{}' WHERE id = {} ".format(key, campo, id))
+                conn.commit()
+        fb = "Cadastro alterado com sucesso"
+        return render_template("edit.html", item=item, fb = fb)
+
+@app.route('/delete', methods = ['POST'])
+def delete():
+    conn = sqlite3.connect('facilitadores.db')
+    c = conn.cursor()
+    # c.execute("SELECT name FROM pragma_table_info('facilitadores')")
+    # keys = c.fetchall()
+    # key_list = []
+    # for i in keys:
+    #     key_list.append(i[0])
+    # c.execute("SELECT * FROM facilitadores")
+    # values = c.fetchall()
+    # facilitadores = []
+    # for row in values:
+    #     item = {}
+    #     for i in range(len(key_list)):
+    #         item[key_list[i]] = row[i]
+    #     facilitadores.append(item)
+    apaga = request.form['id']
+    c.execute("DELETE FROM facilitadores WHERE id = {}".format(apaga))
+    conn.commit()
+    return redirect('/')
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# api = Api(app)
-#
-#
-# class Cadastrar(Resource):
-#     def get(self):
-#         headers = {'Content-Type': 'text/html'}
-#         return make_response(render_template("add.html"),200,headers)
-#
-#     def post(self):
-#         nome = request.form['nome']
-#         email = request.form['email']
-#         cel = request.form['celular']
-#         nasc = request.form['nasc']
-#         conn = sqlite3.connect('jedi.db')
-#         c = conn.cursor()
-#         c.execute("INSERT INTO jedi (nome, email, cel, nasc) VALUES (?,?,?,?)", (nome, email,cel,nasc))
-#         conn.commit()
-#         headers = {'Content-Type': 'text/html'}
-#         return make_response(render_template("add.html", fb = "Jedi Cadastrado"), 200, headers)
-#
-# api.add_resource(Cadastrar, '/')
